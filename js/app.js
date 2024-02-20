5028f24baf259dcdac05207b60c7eac46b60a42e
oye <ogunyemioyetola28@gmail.com>
2024-02-20 02:01:31 +0100

    Fixed the user interface

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var countDownDate = new Date("March, 2024 1:00:00").getTime();
+var countdownfunction = setInterval(function () {
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Update the content of each container
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+})
